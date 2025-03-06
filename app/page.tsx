d70819a590bc1efc513997f33c59efe47347cb0b
Segesp <20200205@lamolina.edu.pe>
2025-03-06 08:57:54 -0500

    Mejoras GOD 10

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/auth.config";
 
 export default async function Home() {
   try {
